be083e15c4b73ec1b43867e1b60eb9a57e838076
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-02 20:53:06 -0500

    complete

---
+++ app/model.js
@@ -0,0 +1,44 @@
+const validate = (state) => {
+  const ret = [];
+
+  if (!state.firstName) {
+    ret.push("First name is required!");
+  }
+
+  if (!state.lastName) {
+    ret.push("Last name is required!");
+  }
+
+  if (!state.username) {
+    ret.push("Username is required!");
+  }
+
+  if (!state.email) {
+    ret.push("Email is required!");
+  }
+
+  if (!state.email?.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+    ret.push("Email is invalid!");
+  }
+
+  if (!state.password) {
+    ret.push("Password is required!");
+  }
+
+  if (state.password?.length < 6) {
+    ret.push("Password must be at least 6 characters!");
+  }
+
+  return ret;
+};
+
+export default {
+  createUser(newUser) {
+    const errors = validate(newUser);
+    if(errors.length) {
+      throw new Error(`User error: ${errors.join(", ")}`);
+    }
+
+  return newUser;
+  },
+};
--- app/router.js
+++ app/router.js
@@ -3,5 +3,25 @@ import Router from "express";
 const router = new Router();
 
 // TODO: Add routes here (maybe 🤔 start with a GET test route)
+router.get("/", async (_, res) => {
+  try {
+    const users = await usersController.index();
+    res.json(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const validatedUser = userModel.createUser(req.body);
+    const newUser = await usersController.add(validatedUser);
+    res.status(201).json(newUser);
+  } catch (error) {
+    req.status(400);
+  }
+});
+
+
 
 export default router;
